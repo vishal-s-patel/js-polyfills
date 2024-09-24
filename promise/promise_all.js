0d3345abad975835e431f78896b258bbf694d622
vishal-s-patel <vishalpatel1002@yahoo.com>
2024-09-24 19:18:57 +0530

    feat: Promise.all polyfill

---
+++ promise/promise_all.js
@@ -0,0 +1,39 @@
+function myPromiseAll(tasks) {
+  let results = [];
+  let completedPromises = 0;
+  return new Promise((resolve, reject) => {
+    tasks.forEach((task, index) => {
+      Promise.resolve(task)
+        .then((result) => {
+          results[index] = result;
+          completedPromises += 1;
+          if (completedPromises === tasks.length) {
+            resolve(results);
+          }
+        })
+        .catch((err) => reject(err));
+    });
+  });
+}
+
+const task1 = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(1), 1000);
+  });
+};
+
+const task2 = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(2), 2000);
+  });
+};
+
+const task3 = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(3), 3000);
+  });
+};
+
+myPromiseAll([task1(), task2(), task3()])
+  .then((result) => console.log("resolved::", result))
+  .catch((err) => console.error("err::", err));
